21173b7671bb2423ed58d34c11457c62bce3b92d
wangbin <pilgrim.bin@gmail.com>
2019-09-11 16:09:55 +0800

    “说怪话。”

--- step2-test-gmm.sh
+++ step2-test-gmm.sh
@@ -9,7 +9,6 @@ VAL_LOG_FILENAME="val_`date +%Y%m%d_%H%M%S`_"${NICKNAME}.log
 # raw 
 #python test.py --name gmm_traintest_new --stage GMM --workers 4 --datamode test --data_list test_pairs.txt --checkpoint checkpoints/gmm_train_new/gmm_final.pth
 
-
 python -u test.py --name gmm_traintest_new \
                   --stage GMM --workers 0 --datamode test \
                   --data_list test_pairs.txt \
